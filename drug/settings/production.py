5a8d5e0842223954d971573cde7474d66b31db45
Elastinex <elastinex@gmail.com>
2020-07-21 14:01:15 +0800

    1

--- drug/settings/production.py
+++ drug/settings/production.py
@@ -11,8 +11,8 @@ ALLOWED_HOSTS = ['10.0.0.153', 'localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'emholboo',
-        'USER': 'emholboo',
+        'NAME': 'pharma',
+        'USER': 'postgres',
         'PASSWORD': '#DiyjoT42u#M',
         'HOST': 'localhost',
         'PORT': '5432'
